3a318858e29a2bf84b6326666b6adcd0ccbb71fe
Gopika Krishnan <gk1656@nyu.edu>
2026-04-22 12:33:28 +0200

    Update Google Script URL in write.php

--- service/write.php
+++ service/write.php
@@ -483,7 +483,7 @@ if ($write_spatial_lev) {
 }
 
 // Send results to Google Sheets
-$googleScriptUrl = 'https://script.google.com/macros/s/AKfycbwSAn8Pp44J37mR4k-i7Vb05EZ3gO5Ko4NR-7En2_1C5oidjH80Z4rEgp_oC2XSTNj3ew/exec';
+$googleScriptUrl = 'https://script.google.com/macros/s/AKfycbyXBO670Z47uImAY14-9fcSHDbBcFSTmeh5bphKzEm6nfkLY8jZAVgfks45H0oRsa0Elw/exec';
 
 foreach ($session->trials as $trial) {
     foreach ($trial->responses as $response) {
