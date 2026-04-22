390c1c56e67794b303b2e889e4b70dff211a684b
Gopika Krishnan <gk1656@nyu.edu>
2026-04-22 12:44:27 +0200

    Implement cURL error handling and logging
    Added error handling and logging for cURL request.

--- service/write.php
+++ service/write.php
@@ -496,13 +496,16 @@ foreach ($session->trials as $trial) {
         ]);
 
         $ch = curl_init($googleScriptUrl);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  
-		curl_setopt($ch, CURLOPT_POST, true);    
-        curl_exec($ch);
-        curl_close($ch);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		$result = curl_exec($ch);
+		$error = curl_error($ch);
+		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+		file_put_contents('../results/debug.txt', "Code: $httpcode | Result: $result | Error: $error\n", FILE_APPEND);
     }
 }
 
